8ee27c5ae519c8aae895695c3b3db3903f4339a3
Christopher Boyd <boyd.christopher.t@gmail.com>
2022-04-30 10:51:24 -0500

    Added some images to the gallery

--- server/modules/gallery.data.js
+++ server/modules/gallery.data.js
@@ -1,5 +1,8 @@
 const galleryItems = [
-    { id: 1, path: 'images/goat_small.jpg', description: 'Photo of a goat taken at Glacier National Park.', likes: 0 }
+    {id: 1, path: 'images/goat_small.jpg', description: 'Photo of a goat taken at Glacier National Park.', likes: 0},
+    {id: 2, path: 'images/ajax.jpg', description: 'Artist\'s representation of .ajax on a very bad day.', likes: 0},
+    {id: 3, path: 'images/relax.jpg', description: 'This is what relaxation looks like.', likes: 0},
+    {id: 4, path: 'images/suspicious.jpg', description: 'Can you feel the love?', likes: 0}
 ];
 
 module.exports = galleryItems;
--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,7 +1,9 @@
-import React from 'react';
+import {useEffect} from 'react';
 import './App.css';
 
 function App() {
+  
+
     return (
       <div className="App">
         <header className="App-header">
